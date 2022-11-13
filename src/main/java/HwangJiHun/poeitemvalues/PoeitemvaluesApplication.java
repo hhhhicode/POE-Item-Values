1b17d03be75816b79a6ae3a497a7e1636a631e7f
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-14 07:21:10 +0900

    First Commit

---
+++ src/main/java/HwangJiHun/poeitemvalues/PoeitemvaluesApplication.java
@@ -0,0 +1,13 @@
+package HwangJiHun.poeitemvalues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeitemvaluesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeitemvaluesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HwangJiHun/poeitemvalues/PoeitemvaluesApplicationTests.java
@@ -0,0 +1,13 @@
+package HwangJiHun.poeitemvalues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeitemvaluesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
