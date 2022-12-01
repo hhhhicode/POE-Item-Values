07b994ae434d824304eced52bd4ae6b2f8329da1
Hwang Ji Hun <hhhhicode@gmail.com>
2022-12-02 01:29:55 +0900

    MyExceptionUtils - @Value Error Fix

--- src/main/java/HwangJiHun/poeitemvalues/model/exception/MyExceptionUtils.java
+++ src/main/java/HwangJiHun/poeitemvalues/model/exception/MyExceptionUtils.java
@@ -1,11 +1,13 @@
 package HwangJiHun.poeitemvalues.model.exception;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.sql.Timestamp;
 
+@Component
 public class MyExceptionUtils {
 
     @Value("${projectId}")
--- src/main/java/HwangJiHun/poeitemvalues/service/ExceptionService.java
+++ src/main/java/HwangJiHun/poeitemvalues/service/ExceptionService.java
@@ -4,25 +4,24 @@ import HwangJiHun.poeitemvalues.domain.feign.ExceptionThrowClient;
 import HwangJiHun.poeitemvalues.model.exception.MyException;
 import HwangJiHun.poeitemvalues.model.exception.MyExceptionUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ExceptionService {
 
-    final
-    ExceptionThrowClient exceptionThrowClient;
+    private final ExceptionThrowClient exceptionThrowClient;
+    private final MyExceptionUtils myExceptionUtils;
 
-    public ExceptionService(ExceptionThrowClient exceptionThrowClient) {
+    public ExceptionService(ExceptionThrowClient exceptionThrowClient, MyExceptionUtils myExceptionUtils) {
         this.exceptionThrowClient = exceptionThrowClient;
+        this.myExceptionUtils = myExceptionUtils;
     }
 
     /**
      * MyException을 지정한 Exception Center로 보냅니다.
      */
     public String sendExceptionToErrorCenter(Exception e) {
-        MyExceptionUtils myExceptionUtils = new MyExceptionUtils();
         MyException myException = myExceptionUtils.getMyException(e);
         String returnValue = exceptionThrowClient.postThrowException(myException);
 
